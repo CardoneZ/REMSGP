f631f2bb4376a6f43e2c8cc52d963535d0e9c1ac
CardoneZ <cardonezapatayt@gmail.com>
2023-04-21 02:49:58 -0500

    Director CRUD

---
+++ src/DAOExceptions/DirectorException.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAOExceptions;
+
+public class DirectorException extends Exception {
+    public DirectorException(String message) {
+        super(message);
+    }
+}
